df2fe4c4df69e009ff0439c6ab49a3661fcf58ff
nadavg <nadavg@yahoo-inc.com>
2015-01-23 15:25:11 -0800

    Initial Commit

---
+++ Rotten_Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten_Tomatoes
+//
+//  Created by Nadav Golbandi on 1/23/15.
+//  Copyright (c) 2015 Nadav Golbandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten_Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten_Tomatoes
+//
+//  Created by Nadav Golbandi on 1/23/15.
+//  Copyright (c) 2015 Nadav Golbandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
